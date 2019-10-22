15556ce1f5644641cf002e177d8cc8f996e51d50
granit <graniitkastrati@gmail.com>
2019-10-22 16:18:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,12 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " " ]
-number_of_turns = 0
-def turn_count(board)
-  
-  board.each do |space|
 
-    if space == "X" || space == "O"
-      number_of_turns += 1
+def turn_count(board)
+  counter = 0
+    board.each do |space|
+
+      if space == "X" || space == "O"
+        counter += 1
+      end
     end
-  end
-  return number_of_turns
+  return counter
 end
